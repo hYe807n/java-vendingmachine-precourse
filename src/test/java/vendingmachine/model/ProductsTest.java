df9ff58007964a3e48bb89ebebc280511872b1d0
hYe807n <rlagkdus55@gmail.com>
2023-11-22 14:54:52 +0900

    test: 잔고보다 구매상품이 비싼 경우, 잔고가 없는 상품 구매할 경우, 예외 처리

--- src/test/java/vendingmachine/model/ProductsTest.java
+++ src/test/java/vendingmachine/model/ProductsTest.java
@@ -17,6 +17,13 @@ class ProductsTest {
         );
     }
 
+    private static Stream<Arguments> exceptionPurchaseProduct() {
+        return Stream.of(
+            Arguments.of("[콜라,1500,1];[사이다,1000,5]", "치킨", 1000, "없는 상품 구매로"),
+            Arguments.of("[콜라,1500,1];[사이다,1000,5]", "콜라", 500, "잔금이 구매상품보다 모자라서")
+        );
+    }
+
     @DisplayName("\"[콜라-1000-4];[사이다+900+1]\"를 판매상품으로 입력하면, 양식 불일치로 오류 처리")
     @Test
     void exceptionProductsForm() {
@@ -28,7 +35,13 @@ class ProductsTest {
     @MethodSource("exceptionPurchaseCase")
     void checkPurchaseState(String product, int balance, boolean state) {
         Products products = new Products(product);
-
         Assertions.assertEquals(products.checkPurchaseState(balance), state);
+    }
+
+    @ParameterizedTest(name = "판매상품 {0}인 상태에서 잔고가 {2}이고, {1}를 구매하면 {3} 오류 처리")
+    @MethodSource("exceptionPurchaseProduct")
+    void checkExceptionPurchaseProduct(String product, String purchaseProduct, int balance, String cause) {
+        Products products = new Products(product);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> products.purchase(purchaseProduct, balance));
     }
 }
