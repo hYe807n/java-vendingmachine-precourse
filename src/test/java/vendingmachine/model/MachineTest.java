bd1beaefd23a9c48e49020c8b24e6d6d410627cb
hYe807n <rlagkdus55@gmail.com>
2023-11-24 09:35:17 +0900

    test: 자판기 투입 금액이 760 원이라면, 각 동전을 최소 개수로 한 잔돈 반환

--- src/test/java/vendingmachine/model/MachineTest.java
+++ src/test/java/vendingmachine/model/MachineTest.java
@@ -1,7 +1,10 @@
 package vendingmachine.model;
 
-
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,5 +14,17 @@ class MachineTest {
     @ValueSource(strings = {"a", "1111"})
     void getInitializeCoin(String money) {
         Assertions.assertThrows(IllegalArgumentException.class,() -> new Machine(money));
+    }
+
+    @DisplayName("자판기 투입 금액이 760 원이라면, 각 동전을 최소 개수로 한 잔돈 반환")
+    @Test
+    void checkBalanceResult() {
+        Map<Integer, Integer> resultCoin = new LinkedHashMap<>();
+        resultCoin.put(500, 1);
+        resultCoin.put(100, 2);
+        resultCoin.put(50, 1);
+        resultCoin.put(10, 1);
+
+        Assertions.assertEquals(new Machine("1000000000").resultCoins(760), resultCoin);
     }
 }
