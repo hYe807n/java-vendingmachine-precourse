615eec0629eb8a26dad7965056fe315fff6c710e
hYe807n <rlagkdus55@gmail.com>
2023-11-24 09:58:51 +0900

    test: 인자에 숫자 이외의 문자가 포함되어있다면 IllegalArgumentException 발생

---
+++ src/test/java/vendingmachine/exception/ValidationTest.java
@@ -0,0 +1,47 @@
+package vendingmachine.exception;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ValidationTest {
+
+    @DisplayName("인자에 숫자 이외의 문자가 포함되어있다면 IllegalArgumentException 발생")
+    @Test
+    void exceptionByContainsString() {
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() ->
+            Validation.composeOnlyNumber("12a"));
+    }
+
+    @Test
+    void checkMoneyRange() {
+    }
+
+    @Test
+    void checkMoneyDivide() {
+    }
+
+    @Test
+    void checkProductForm() {
+    }
+
+    @Test
+    void composeOnlyKorean() {
+    }
+
+    @Test
+    void checkProductCount() {
+    }
+
+    @Test
+    void noneExistPurchaseProduct() {
+    }
+
+    @Test
+    void deficiencyPurchaseBalance() {
+    }
+
+    @Test
+    void zeroCountProduct() {
+    }
+}
